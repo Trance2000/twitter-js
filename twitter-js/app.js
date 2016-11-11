7e8f4f3ac099f2ca149e7a48ce7eff86c3a9da20
Artoym Toupchienko <artoymt1990@gmail.com>
2016-11-10 21:30:23 -0500

    set up engine render w/ nunjucks

--- twitter-js/app.js
+++ twitter-js/app.js
@@ -1,11 +1,70 @@
 const express = require("express");
+const nunjucks = require("nunjucks");
 const app = express();
 
 
-app.listen( 3000, function(){
-  console.log("server listening");
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+
+var conf = nunjucks.configure("views", {noCache: true});
+nunjucks.render("index.html", locals, function(err, output) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(output);
+  }
 });
 
+
+
+app.engine("html", nunjucks.render);
+app.set("view engine", "html");
+
+
+
+
+app.use(function(req, res, next) {
+  console.log("hello");
+  next();
+});
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  // const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render( 'index', locals );
+});
+
+app.get('/news', function (req, res) {
+  res.send("NEWS");
+});
+
+// app.use(function(req, res, next) {
+//   console.log('welcome');
+//   next();
+// });
+
+app.get('/is-anybody-in-there', function (req, res, next) {
+  res.send('YES!!');
+
+});
+
+// app.get('/is-anybody-in-there/special', function (req, res, next) {
+//   res.send('this is a special area');
+//   next();
+// });
+
+// app.post('/is-anybody-in-there', function (req, res, next) {
+//   req.send("hi");
+//   res.send("hello");
+// });
+
+app.listen( 3000, function(){
+  console.log("server listening");
 });
